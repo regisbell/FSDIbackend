ed7ae0e37b636f8c77ec70f6f9d584b36201c972
Regis <regis.a.bell@gmail.com>
2022-01-26 22:34:06 -0500

    FSDI 110 #3 mongo connection

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import pymongo 
+import certifi
 
 mongo_url = "mongodb+srv://class:4asYZandEhrSbJIm@cluster0.ikguw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient(mongo_url)
+client = pymongo.MongoClient(mongo_url, tlsCAFile=certifi.where())
 
 db = client.get_database("StoreRegis")
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 
+from unittest import result
 from flask import Flask, request, abort
 from mock_data import catalog
 import json 
 import random
 from config import db
 from flask_cors import CORS
+from bson import ObjectId 
 
 app = Flask(__name__)
 CORS(app) # *DANGER* anyone can connect to this server
@@ -46,10 +48,13 @@ def practice():
 
 @app.route("/api/catalog")
 def get_catalog():
-    test = db.products.find({})
-    print(test)
+    cursor = db.products.find({})
+    results = []
+    for product in cursor:
+        product['_id'] = str(product['_id'])
+        results.append(product)
 
-    return json.dumps(catalog)
+    return json.dumps(results)
 
 
 @app.route("/api/catalog", methods=["post"])
@@ -57,28 +62,27 @@ def save_product():
     product = request.get_json()
     print (product)
 
-    # assign unique _id
-    
-
-    # save the product n the catalog
     catalog.append(product)
 
-    
     db.products.insert_one(product)
 
     print("-----SAVED-----")
     print(product)
+
+    product['_id'] = str(product['_id'])
 
     return  json.dumps(product)
 
 @app.route("/api/cheapest")
 def get_cheapest():
     # find the cheapest product on the catalog list 
-    cheapest = catalog[0]
-    for product in catalog:
+    cursor = db.products.find({})
+    cheapest = cursor[0]
+    for product in cursor:
         if product["price"] < cheapest["price"]:
             cheapest = product
     
+    cheapest['_id'] = str(cheapest['_id'])
 
     # return it as json
     return json.dumps(cheapest)
@@ -86,17 +90,19 @@ def get_cheapest():
 
 @app.route("/api/product/<id>")
 def get_product(id):
-    # find the product whose _id is equal to id
-    for product in catalog:
-        if product["_id"] == id:
-            return json.dumps(product)
+    if(not ObjectId.is_valid(id)):
+        return abort(400, "ID is not a valid ObjectId")
+        
+   result = db.products.find_one({'_id': ObjectId(id)})
+   
+   if not result:
+       return abort(404)
 
-    #return it as json
-    return "NOT FOUND"
+   result['_id'] = str(result['_id'])
 
-# endpoint to retrieve all the products by category
-# you should receive the cat. name,
-# return all the products that belong to that category
+   return json.dumps(result)
+
+
 @app.route("/api/catalog/<category>")
 def get_by_category(category):
     result = []
